b45e77dcc6c3f32f99c3ec69c931600d8fbba241
Eun Oh <160467375+eunohhh@users.noreply.github.com>
2024-05-24 16:54:30 +0900

    [level 1] Title: 소수 만들기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,26 @@
+function solution(nums) {
+    var answer = 0;
+    
+    function isPrime(num) {
+	    if(num === 1) return false; 
+        // Math.sqrt 함수를 사용하여 제곱근까지만 반복하도록 한다.
+        for(let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+            if(num % i === 0) return false;
+        } 
+        return true; 
+    }
+    const n = nums.length;
+    
+    for (let i = 0; i < n - 2; i++) {
+        for (let j = i + 1; j < n - 1; j++) {
+            for (let k = j + 1; k < n; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if(isPrime(sum)){
+                    answer ++;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
