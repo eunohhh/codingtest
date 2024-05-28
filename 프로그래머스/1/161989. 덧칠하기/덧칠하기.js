81bc4e843ce7b6f3ba55c466dedb83f6fa6bad16
Eun Oh <160467375+eunohhh@users.noreply.github.com>
2024-05-28 14:34:58 +0900

    [level 1] Title: 덧칠하기, Time: 3.39 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,24 +1,4 @@
 function solution(n, m, section) {
-//     if(section.length === 1) return 1;
-    
-//     const area = section[section.length - 1] - section[0] + 1;
-//     let count = 1; 
-//     let remain = area - m;
-//     let temp = true;
-    
-//     if(remain < m && remain > 0){
-//         count++;
-//     }else if(remain < 0){
-//         return count;
-//     }else{
-//         while(temp){
-//             remain = remain - m;
-//             count++;
-//             if(remain < m) temp = false;
-//         }
-//     }
-        
-//     return count;
     let answer = 0;
     let painted = 0;
     for(const sec of section) {
