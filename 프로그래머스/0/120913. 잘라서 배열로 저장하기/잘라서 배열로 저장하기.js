fab4525194172d41c4bda4cf877f52ab800ad636
Eun Oh <160467375+eunohhh@users.noreply.github.com>
2025-01-22 16:48:20 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,24 +1,30 @@
 function solution(my_str, n) {
-    let before = 0;
-    let after = n;
-    const strArray = [...my_str];
     let answer = [];
-    let temp = '';
-
-    strArray.forEach((str, idx) => {
-        if(idx >= before && idx < after){
-            temp += str;
-        }
-        if(idx === after){
-            answer.push(temp);
-            temp = '';
-            before = after;
-            after = after + n;
-            temp += str;
-        }
-        if(idx === strArray.length - 1){
-            answer.push(temp);
-        }
-    })
+    for(let i = 0; i < my_str.length; i+=n){
+        answer.push(my_str.substring(i, i+n))
+    }
     return answer;
+    
+//     let before = 0;
+//     let after = n;
+//     const strArray = [...my_str];
+//     let answer = [];
+//     let temp = '';
+
+//     strArray.forEach((str, idx) => {
+//         if(idx >= before && idx < after){
+//             temp += str;
+//         }
+//         if(idx === after){
+//             answer.push(temp);
+//             temp = '';
+//             before = after;
+//             after = after + n;
+//             temp += str;
+//         }
+//         if(idx === strArray.length - 1){
+//             answer.push(temp);
+//         }
+//     })
+//     return answer;
 }
